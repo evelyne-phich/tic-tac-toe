eeee6cd9cad5bada63c65d74ffe2b31196667a51
Evelyne PHICH <evelyne.phich@gmail.com>
2021-02-07 21:48:53 +0100

    create node project

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+const PORT = process.env.PORT || 3002;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+app.get("/", (request, response) => {
+  response.render("index", {});
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is listening on: http://localhost:${PORT}`);
+});
